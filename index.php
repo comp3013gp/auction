5ce9d8a67591e7a51a7472d180a2b9f89d23a612
takeaki <ty.jaw6@gmail.com>
2016-02-14 11:59:36 +0000

    downloaded jquery and bootstrap

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+  require_once("public_html/top_bar.php")
+?>
+<?php
+  require_once("public_html/bottom_bar.php")
+?>
---
+++ public_html/bottom_bar.php
@@ -0,0 +1,4 @@
+  <script src="resources/library/jquery-1.11.3.min.js"></script>
+  <script src="resources/library/bootstrap-3.3.6-dest/bootstrap.min.js"></script>
+</body>
+</html>
