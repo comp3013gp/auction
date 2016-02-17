4fa1df85c5ff48cf1768136481fc68569447d7ce
takeaki <ty.jaw6@gmail.com>
2016-02-17 16:07:48 +0000

    modified paths in config.php

--- public_html/login.php
+++ public_html/login.php
@@ -1,6 +1,7 @@
 <?php
-  require_once("../resources/templates/top_bar.php");
+  require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
+  require_once(TEMPLATES_PATH . '/top_bar.php');
 ?>
 <?php
-  require_once("../resources/templates/bottom_bar.php");
+  require_once(TEMPLATES_PATH . '/bottom_bar.php');
 ?>
--- resources/config.php
+++ resources/config.php
@@ -1,4 +1,13 @@
 <?php
+  defined("CSS_PATH")
+    or define("CSS_PATH", realpath(dirname(__FILE__) . '../public_html/css/'));
+  defined("JS_PATH")
+    or define("JS_PATH", realpath(dirname(__FILE__) . '../public_html/js/'));
+  defined("LIBRARY_PATH")
+    or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library/'));
+  defined("TEMPLATES_PATH")
+    or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates/'));
+
   ini_set("error_reporting", "true");
   error_reporting(E_ALL|E_STRCT);
 ?>
