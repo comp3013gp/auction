08d32e211a079f18cc42b1ccf2e2de478dea2b44
takeaki <ty.jaw6@gmail.com>
2016-02-17 22:17:46 +0000

    created login.php

--- public_html/login.php
+++ public_html/login.php
@@ -2,6 +2,16 @@
   require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
   require_once(TEMPLATES_PATH . '/top_bar.php');
 ?>
+<!-- login page -->
+<h1>
+  LogIn
+</h1>
+<!-- signup page -->
+<!--
+<h1>
+  SignUp
+</h1>
+
 <?php
   require_once(TEMPLATES_PATH . '/bottom_bar.php');
 ?>
