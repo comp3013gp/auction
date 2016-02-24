129688e5c86b81f52e6fe68c78b5ef6a867d6527
takeaki <ty.jaw6@gmail.com>
2016-02-24 18:02:14 +0000

    commit

--- database.sql
+++ database.sql
@@ -1,30 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 4.4.10
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Feb 13, 2016 at 04:45 PM
--- Server version: 5.5.42
--- PHP Version: 7.0.0
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `auction_site`
---
-
 CREATE DATABASE `auction_site`;
 USE `auction_site`;
 
--- --------------------------------------------------------
+CREATE TABLE `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_type` enum('buyer','seller') NOT NULL,
+  `name` varchar(16) NOT NULL INDEX,
+  `email_address` varchar(255) NOT NULL INDEX,
+  `password` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `auction`
---
+CREATE TABLE `bid` (
+  `bid_id` int(10) unsigned not null AUTO_INCREMENT,
+  `bidder_id` int(10) unsigned NOT NULL,
+  `auction_id` int(10) unsigned NOT NULL,
+  `time` datetime NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `auction` (
-  `auction_id` int(10) unsigned NOT NULL,
+  `auction_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `seller_id` int(10) unsigned NOT NULL,
   `item_id` int(10) unsigned NOT NULL,
   `start_price` decimal(10,2) NOT NULL,
@@ -33,123 +27,53 @@ CREATE TABLE `auction` (
   `views` int(10) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `bid`
---
-
-CREATE TABLE `bid` (
-  `bid_id` int(10) unsigned NOT NULL,
-  `bidder_id` int(10) unsigned NOT NULL,
-  `auction_id` int(10) unsigned NOT NULL,
-  `time` datetime NOT NULL,
-  `price` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
-CREATE TABLE `category` (
-  `category_id` int(10) unsigned NOT NULL,
-  `name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `item`
---
-
 CREATE TABLE `item` (
-  `item_id` int(10) unsigned NOT NULL,
+  `item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `owner_id` int(10) unsigned NOT NULL,
   `name` varchar(50) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `category_id` int(10) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `rating`
---
+CREATE TABLE `category` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `rating` (
-  `rating_id` int(10) unsigned NOT NULL,
+  `rating_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `rating` enum('0','1','2','3','4','5') NOT NULL,
   `comment` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `user_id` int(10) unsigned NOT NULL,
-  `user_type` enum('buyer','seller') NOT NULL,
-  `name` varchar(16) NOT NULL,
-  `email_address` varchar(255) NOT NULL,
-  `password` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `auction`
---
 ALTER TABLE `auction`
   ADD PRIMARY KEY (`auction_id`),
   ADD KEY `seller_id` (`seller_id`),
   ADD KEY `item_id` (`item_id`);
 
---
--- Indexes for table `bid`
---
 ALTER TABLE `bid`
   ADD PRIMARY KEY (`bid_id`),
   ADD KEY `bidder_id` (`bidder_id`),
   ADD KEY `auction_id` (`auction_id`);
 
---
--- Indexes for table `category`
---
 ALTER TABLE `category`
   ADD PRIMARY KEY (`category_id`);
 
---
--- Indexes for table `item`
---
 ALTER TABLE `item`
   ADD PRIMARY KEY (`item_id`),
   ADD KEY `owner_id` (`owner_id`),
   ADD KEY `category_id` (`category_id`);
 
---
--- Indexes for table `rating`
---
 ALTER TABLE `rating`
   ADD PRIMARY KEY (`rating_id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
+<<<<<<< Updated upstream
 --
 -- AUTO_INCREMENT for table `auction`
 --
@@ -217,5 +141,7 @@ ALTER TABLE `rating`
 
 USE `mysql`;
 
+=======
+>>>>>>> Stashed changes
 GRANT ALL PRIVILEGES ON *.* TO 'comp3013-gp'@'localhost' IDENTIFIED BY PASSWORD '*85FBE1F41A28D0D54A8489CD4FBE0FEBA476DDB4' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON `auction_site`.* TO 'comp3013-gp'@'localhost' WITH GRANT OPTION;
