2878aec7d2fa9b90c9295168d1020eccaa83e9c4
takeaki <ty.jaw6@gmail.com>
2016-02-29 09:41:51 +0000

    modified main page

--- public_html/main.php
+++ public_html/main.php
@@ -4,18 +4,20 @@
   if (!isset($_SESSION['user_id'])) {
     header("Location: /auction/public_html/login.php");
   }
+  
+  require_once(realpath(dirname(__FILE__) . "/../resources/dbconnection.php"));
   require_once(realpath(dirname(__FILE__) . "/../resources/config.php"));
+
   require_once(TEMPLATES_PATH . '/top_bar.php');
 ?>
 <h1>
-  Main page
+  Home
 </h1>
-<span>Hi user <?php echo $_SESSION["user_id"]?></span>
 <?php
   if ($_SESSION['user_type'] == "buyer") {
-    echo "<a href='/auction/public_html/search.php'>Search Auction</a>";
+    echo "<a class='main-page' href='/auction/public_html/search.php'>Search Auction</a>";
   } else {
-    echo "<a href='/auction/public_html/new_auction.php'>Create Auction</a>";
+    echo "<a class='main-page' href='/auction/public_html/new_auction.php'>Create Auction</a>";
   }
 ?> 
 <?php
