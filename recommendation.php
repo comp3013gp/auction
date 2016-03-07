a317b05b235bba9a0b444edaca43800fc6c2a172
Zhengyi Yang <zhengyi.yang@outlook.com>
2016-03-07 15:38:42 +0000

    fix recommendation

--- recommendation.php
+++ recommendation.php
@@ -1,7 +1,7 @@
 <?php
   
   require_once(__DIR__ ."/resources/dbconnection.php");
-  require_once(__DIR__ . "/resources/email.php"));
+  require_once(__DIR__ . "/resources/email.php");
 
   function recommend($user_id){
 	  $query = "SELECT * FROM auction WHERE
@@ -10,7 +10,7 @@
 	  WHERE bid.bidder_id <> $user_id AND bid.auction_id IN(
       SELECT bid.auction_id FROM bid WHERE bid.bidder_id = $user_id
       GROUP BY bid.auction_id) GROUP BY bid.bidder_id ) GROUP BY
-	  bid.auction_id ) AND auction.has_ended = 0 LIMIT 10;"
+	  bid.auction_id ) AND auction.has_ended = 0 LIMIT 10;";
 	  
 	  $recommends = mysqli_query($connection, $query);
 	  if(empty($recommends)){
@@ -24,14 +24,18 @@
 	  return $str_recommends;
   }
   
-  $query="SELECT user_id,email_address from user"
+  $query="SELECT user_id,email_address from user";
   $users = mysqli_query($connection, $query);
   $sender = new email_sender();
+  $counter=0;
   while ($user = mysqli_fetch_array($users)) {
 		  $str_recommends = recommend($user['user_id']);
 		  if ($str_recommends!=''){
-			  $sender->send($user['email_address'],$str_recommends)
+			  $counter++;
+			  $sender->send($user['email_address'],$str_recommends);
 		  }
  }
+ 
+ echo(date("Y-m-d H:i:s")." recommendation.php : $counter emails sent.\n");
         
 ?>
