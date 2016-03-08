c8a6f465c16efae06b758a13a00642e9375b4290
Zhengyi Yang <zhengyi.yang@outlook.com>
2016-03-08 22:59:22 +0000

    create more email accounts

--- resources/email.php
+++ resources/email.php
@@ -5,42 +5,63 @@ require_once("library/PHPMailer/PHPMailerAutoload.php");
 class email_sender
 {
 
-    private $mail;
+    private $mailers = array();
+    private $accounts = [ 'comp3013@outlook.com','comp3013-11@outlook.com','comp3013-11-1@outlook.com','comp3013-11-2@outlook.com','comp3013-11-3@outlook.com'];
+    private $error_info;
 
     function __construct()
     {
-        $this->mail = new PHPMailer;
+        shuffle($this->accounts);
+        $this->create_senders();
+    }
 
-        $this->mail->isSMTP();
-        $this->mail->Host = 'smtp-mail.outlook.com';
-        $this->mail->SMTPAuth = true;
-        $this->mail->Username = 'comp3013-11@outlook.com';
-        $this->mail->Password = 'UCLdatabase';
-        $this->mail->SMTPSecure = 'tls';
-        $this->mail->Port = 587;
-
-        $this->mail->setFrom('from@example.com', 'Mailer');
+    function create_senders()
+    {
+        for ($i = 0; $i < count($this->accounts); $i++) {
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+            $mail->Host = 'smtp-mail.outlook.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = $this->accounts[$i];
+            $mail->Password = 'UCLdatabase';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+            $mail->setFrom('comp3013-1@outlook.com', 'comp3013-11');
+            $this->mailers[$i] = $mail;
+        }
     }
 
     function send($recipient, $subject, $html_body)
     {
-        $this->mail->ClearAllRecipients();
-        $this->mail->addAddress($recipient);
+        foreach ($this->mailers as $mailer) {
+            $mailer->ClearAllRecipients();
+            $mailer->addAddress($recipient);
 
-        $this->mail->isHTML(true);
+            $mailer->isHTML(true);
 
-        $this->mail->Subject = $subject;
-        $this->mail->Body = $html_body;
-        //$this->mail->AltBody = $txt_body;
+            $mailer->Subject = $subject;
+            $mailer->Body = $html_body;
+            //$mailer->AltBody = $txt_body;
+            if ($mailer->send()) {
+                return true;
+            } else {
+                $this->error_info = $this->error_info . $mailer->Sender . " : " . $mailer->ErrorInfo . " \n";
+            }
+        }
 
-        return $this->mail->send();
+
+        return false;
     }
 
-    function get_error_info(){
-        return $this->mail->ErrorInfo;
+    function get_error_info()
+    {
+        return $this->error_info;
     }
 }
 
-//$sender=new email_sender();
-//$sender->send('ganzhexiaxiaohao@163.com','test','body');
-//echo 'ok';
+//$sender = new email_sender();
+//if ($sender->send('ganzhexiaxiaohao@163.com', 'comp3013-test', 'comp3013-test body')) {
+//    echo 'ok';
+//} else {
+//    echo $sender->get_error_info();
+//}
