7ec66aeb09428d0b8ec263a9060edd0a7024d915
takeaki <ty.jaw6@gmail.com>
2016-03-08 19:45:08 +0000

    work in progress

--- recommendation.php
+++ recommendation.php
@@ -1,9 +1,9 @@
 <?php
   
-  require_once(__DIR__ ."/resources/dbconnection.php");
-  require_once(__DIR__ . "/resources/email.php");
+  require_once(realpath(dirname(__FILE__) . "/resources/dbconnection.php"));
+  require_once(realpath(dirname(__FILE__) . "/resources/email.php"));
 
-  function recommend($user_id){
+  function recommend($dbconnection, $user_id){
 	  $query = "SELECT * FROM auction WHERE
 	  auction.auction_id IN( SELECT bid.auction_id FROM bid
 	  WHERE bid.bidder_id IN( SELECT bid.bidder_id FROM bid
@@ -12,7 +12,7 @@
       GROUP BY bid.auction_id) GROUP BY bid.bidder_id ) GROUP BY
 	  bid.auction_id ) AND auction.has_ended = 0 LIMIT 10;";
 	  
-	  $recommends = mysqli_query($connection, $query);
+	  $recommends = mysqli_query($dbconnection, $query);
 	  if(empty($recommends)){
 		  return '';
 	  }
@@ -29,10 +29,10 @@
   $sender = new email_sender();
   $counter=0;
   while ($user = mysqli_fetch_array($users)) {
-		  $str_recommends = recommend($user['user_id']);
+		  $str_recommends = recommend($connection, $user['user_id']);
 		  if ($str_recommends!=''){
 			  $counter++;
-			  $sender->send($user['email_address'],$str_recommends);
+			  $sender->send($user['email_address'],'Recommended Items for You!!',$str_recommends);
 		  }
  }
  
