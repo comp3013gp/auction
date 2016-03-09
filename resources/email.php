61817e351f41869166256afca41e2b4dbb096baf
Zhengyi Yang <zhengyi.yang@outlook.com>
2016-03-09 15:22:28 +0000

    use php mail() instead of phpmailer

--- resources/email.php
+++ resources/email.php
@@ -1,65 +1,28 @@
 <?php
 
-require_once("library/PHPMailer/PHPMailerAutoload.php");
-
 class email_sender
 {
 
-    private $mailers = array();
-    private $accounts = [ 'comp3013@outlook.com','comp3013-11@outlook.com','comp3013-11-1@outlook.com','comp3013-11-2@outlook.com','comp3013-11-3@outlook.com'];
-    private $error_info;
-
+	private $headers;
+	
     function __construct()
     {
-        $this->create_senders();
-    }
-
-    function create_senders()
-    {
-        for ($i = 0; $i < count($this->accounts); $i++) {
-            $mail = new PHPMailer;
-            $mail->isSMTP();
-            $mail->Host = 'smtp-mail.outlook.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = $this->accounts[$i];
-            $mail->Password = 'UCLdatabase';
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
-            $mail->setFrom('comp3013-1@outlook.com', 'comp3013-11');
-            $this->mailers[$i] = $mail;
-        }
+		$this->headers = "MIME-Version: 1.0" . "\r\n";
+		$this->headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     }
 
     function send($recipient, $subject, $html_body)
     {
-        shuffle($this->mailers);
-        foreach ($this->mailers as $mailer) {
-            $mailer->ClearAllRecipients();
-            $mailer->addAddress($recipient);
-
-            $mailer->isHTML(true);
-
-            $mailer->Subject = $subject;
-            $mailer->Body = $html_body;
-            //$mailer->AltBody = $txt_body;
-            if ($mailer->send()) {
-                return true;
-            } else {
-                $this->error_info = "  ".$this->error_info . $mailer->Sender . " : " . $mailer->ErrorInfo . " \n";
-            }
-        }
-
-        return false;
+		return mail($recipient, $subject, $html_body,$this->headers);
     }
 
     function send_with_log($recipient, $subject, $html_body){
         echo "  Sending email to $recipient ";
         $flag=$this->send($recipient, $subject, $html_body);
-        if($flag){
+        if($flag!=false){
             echo " -- OK \n";
         }else{
             echo " -- ERROR \n";
-            echo $this->get_error_info();
         }
 
     }
@@ -69,6 +32,3 @@ class email_sender
         return $this->error_info;
     }
 }
-
-//$sender = new email_sender();
-//$sender->send_with_log('ganzhexiaxiaohao@163.com', 'comp3013-test', 'comp3013-test body');
